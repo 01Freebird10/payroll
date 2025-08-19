83151d23fac3e7fef75838e969242aedcfa594f7
Iniyan S K <skiniyan44@gmail.com>
2025-08-19 22:35:34 +0530

    Add feature change

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,6 @@
 import unittest
 class TestSample(unittest.TestCase):
-    def test_example(self):
+    def test_examples(self):
         self.assertTrue(True)  # Simple passing test
 if __name__ == '__main__':
     unittest.main()
