e87c2f34eeeea5c71e26d22b1c227372f71cf9b8
Iniyan S K <skiniyan44@gmail.com>
2025-08-19 22:21:23 +0530

    Add tests folder and sample test

---
+++ tests/test_sample.py
@@ -0,0 +1,6 @@
+import unittest
+class TestSample(unittest.TestCase):
+    def test_example(self):
+        self.assertTrue(True)  # Simple passing test
+if __name__ == '__main__':
+    unittest.main()
